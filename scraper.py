d3fc76ca27dc0cff8a72750c7fd817f421fd4c26
Unknown <rmacwhor@uci.edu>
2020-10-27 13:08:37 -0700

    Implemented basic crawler functionality
    This one is bound to make mistakes

--- scraper.py
+++ scraper.py
@@ -1,18 +1,28 @@
 import re
 from urllib.parse import urlparse
+from bs4 import BeautifulSoup
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
 
 def extract_next_links(url, resp):
-    # Implementation requred.
-    return list()
+    # VERY BASIC IMPLEMENTATION, only for testing
+    soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
+    return [link.get('href') for link in soup.find_all('a')]
 
 def is_valid(url):
     try:
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
+            return False
+        if ('today.uci.edu' in parsed.netloc and
+            'department/information_computer_sciences' not in parsed.path):
+            return False
+        valid_domains = ['ics.uci.edu', 'cs.uci.edu',
+                         'informatics.uci.edu', 'stat.uci.edu']
+        # if the url's domain doesn't include any of these valid domains
+        if not any(domain in parsed.netloc for domain in valid_domains):
             return False
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
